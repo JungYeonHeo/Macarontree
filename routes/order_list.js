1bda0cc97754b52088b87339b7fdcf79469de5c6
golttaeline <heojungyeon.97@gmail.com>
2021-12-16 09:40:21 +0900

    [macarontree] * 탭간격 수정
    ; 탭간격 2인 파일을 탭간격 4로 변경

--- routes/order_list.js
+++ routes/order_list.js
@@ -10,22 +10,22 @@ var mongodb_connection = require('../db/mongodb_conn');
 
 // 전달할 데이터 객체 생성
 var send_data = {};
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  mongodb_connection.connectToServer(function(err, client) {
-    if (err) console.log(">>> MongoDB 접속 중 에러 발생함 - " +  err);
+	mongodb_connection.connectToServer(function (err, client) {
+		if (err) console.log(">>> MongoDB 접속 중 에러 발생함 - " + err);
 
-    var mongodb = mongodb_connection.getDb();
-    mongodb.collection("order").find({ord_id: req.session.user_id}).sort({'ord_date': -1}).toArray(function(err, result) {
-      if (err) console.log(">>> 주문 내역 조회 중 에러 발생함 - " +  err);
+		var mongodb = mongodb_connection.getDb();
+		mongodb.collection("order").find({ ord_id: req.session.user_id }).sort({ 'ord_date': -1 }).toArray(function (err, result) {
+			if (err) console.log(">>> 주문 내역 조회 중 에러 발생함 - " + err);
 
-      console.log("### 주문 내역 조회");
-      console.log(result);
-      send_data.order = result;
+			console.log("### 주문 내역 조회");
+			console.log(result);
+			send_data.order = result;
 
-      res.render('../views/order_list.ejs', send_data);
-    });
-  });
+			res.render('../views/order_list.ejs', send_data);
+		});
+	});
 });
 
 
