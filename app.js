47c276c45869e4d23735fcf50d7ff664322779f2
golttaeline <heojungyeon.97@gmail.com>
2021-12-17 14:47:03 +0900

    [macarontree] * 회원가입 형식 수정
    ; 아이디 이메일 형식으로 변경
    ; 아이디 중복체크 다른 버튼으로 분리
    ; 아이디 중복체크 페이지 이름 변경 (id_check.js -> signup_id_check.js)
    ; 입력칸 모두를 하나의 경고메시지로 보여줬던 것을 입력칸 마다 각각의 경고 메시지로 보여줄 수 있도록 수정
    ; 아이디, 비밀번호, 전화번호 형식이 맞는지 체크
    ; 카카오 주소 api를 통한 주소입력
    ; 바뀐 ui에 대한 반응형 수정
    ; 탭간격 2인 파일을 탭간격 4로 변경

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ var cookieParser = require('cookie-parser');
 // 로그
 var logger = require('morgan');
 
-// session 에서 사용
+// session
 var session = require('express-session');
 const FileStore = require('session-file-store')(session);
 
@@ -50,7 +50,7 @@ var order = require('./routes/order'); // unique 페이지 주문처리
 var like_add_delete = require('./routes/like_add_delete');  // Basic 상품 찜 추가 
 var login = require('./routes/login'); // 로그인 페이지 
 var login_check = require('./routes/login_check');  // 로그인 정보 체크
-var id_check = require('./routes/id_check'); // 회원가입 id 중복 체크
+var signup_id_check = require('./routes/signup_id_check'); // 회원가입 id 중복 체크
 var signup_agree = require('./routes/signup_agree'); // 회원가입 약관동의 페이지 
 var signup_input = require('./routes/signup_input'); // 회원가입 사용자 정보 입력 페이지 
 var signup_add = require('./routes/signup_add'); // 사용자 추가 
@@ -80,20 +80,20 @@ app.use(cookieParser());
 
 // 세션
 app.use(session({
-  key: 'sid',
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true,
-  store: new FileStore(), //sessions라는 디렉토리가 생김
-  cookie: {
-    maxAge: 24000 * 60 * 60 // 쿠키 유효기간 24시간
-  }
+	key: 'sid',
+	secret: 'secret',
+	resave: false,
+	saveUninitialized: true,
+	store: new FileStore(), //sessions라는 디렉토리가 생김
+	cookie: {
+		maxAge: 24000 * 60 * 60 // 쿠키 유효기간 24시간
+	}
 }));
 
 // body-parser 설정
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
- 
+
 // public 폴더를 static으로 오픈
 app.use('/public', static(path.join(__dirname, 'public')));
 app.use('/uploads', static(path.join(__dirname, 'uploads')));
@@ -103,14 +103,14 @@ app.use(cors());
 
 // 로그인 여부 확인
 app.use(function (req, res, next) {
-  if (req.session.logined) {
-    console.log('### res.locals.isLogin: yes');
-    res.locals.isLogin = true;
-  } else {
-    console.log('### res.locals.isLogin: no');
-    res.locals.isLogin = false;
-  }
-  next();
+	if (req.session.logined) {
+		console.log('### res.locals.isLogin: yes');
+		res.locals.isLogin = true;
+	} else {
+		console.log('### res.locals.isLogin: no');
+		res.locals.isLogin = false;
+	}
+	next();
 });
 
 // 기능별 라우팅 관리
@@ -121,32 +121,32 @@ app.use('/contest', contest);
 app.use('/cooking', cooking);
 app.use('/mind', mind);
 app.use('/order', order);
-app.use('/like_add_delete', like_add_delete); 
-app.use('/login', login); 
-app.use('/id_check', id_check);
+app.use('/like_add_delete', like_add_delete);
+app.use('/login', login);
 app.use('/login_check', login_check);
-app.use('/signup_agree', signup_agree); 
-app.use('/signup_input', signup_input); 
-app.use('/signup_add', signup_add); 
-app.use('/order_done', order_done); 
-app.use('/order_list', order_list); 
-app.use('/like_list', like_list); 
-app.use('/like_list_delete', like_list_delete); 
-app.use('/user_update', user_update); 
-app.use('/user_updateDone', user_updateDone); 
+app.use('/signup_id_check', signup_id_check);
+app.use('/signup_agree', signup_agree);
+app.use('/signup_input', signup_input);
+app.use('/signup_add', signup_add);
+app.use('/order_done', order_done);
+app.use('/order_list', order_list);
+app.use('/like_list', like_list);
+app.use('/like_list_delete', like_list_delete);
+app.use('/user_update', user_update);
+app.use('/user_updateDone', user_updateDone);
 
-//404 에러 페이지 처리
+// 404 에러 페이지 처리
 var errorHandler = expressErrorHandler({
-  static: {
-  //    '404': './views/404.ejs'
-  }
+	static: {
+		//    '404': './views/404.ejs'
+	}
 });
 
 app.use(expressErrorHandler.httpError(404));
 app.use(errorHandler);
 
 // 웹서버 시작
-var server = http.createServer(app).listen(app.get('port'), function(){
-  console.log('### 웹 서버 시작됨 -> %s', server.address());
+var server = http.createServer(app).listen(app.get('port'), function () {
+	console.log('### 웹 서버 시작됨 -> %s', server.address());
 });
 
---
+++ routes/signup_id_check.js
@@ -0,0 +1,60 @@
+/**
+ * 회원가입시 id 중복을 막기 위한 id 중복을 체크
+ * 
+ */
+
+var http = require('http');
+var crypto = require('crypto');
+var express = require('express');
+var router = express.Router();
+
+var connection = require('../db/db_conn')();
+var db_conn = connection.init();
+    
+var result;
+router.post('/', function(req, res) {
+    console.log("### id_check 호출됨.");
+     
+    console.log(req.body);
+    let id = req.body.id;
+    db_conn.getConnection(function(err, conn) {
+        if (err) {
+        	if (conn) {
+                conn.release();
+            }
+            msg = "사용자 로그인 중 에러 발생";
+            console.log(">>> 사용자 로그인 중 에러 발생 - " + err);
+            result = 0;
+			return;          
+        }   
+        console.log("데이터베이스 연결 스레드 아이디 : " + conn.threadId);
+
+        // 데이터를 객체로 만듦
+        
+        // id  중복 체크 
+        var exec = conn.query('select * from member where m_id = ? ', [id], function(err, dbdata) {
+        	console.log("실행 대상 SQL : " + exec.sql);
+        	
+        	if (err) {
+        		console.log(">>> id 중복 체크 중 SQL 실행 시 에러 발생함 - " + err);
+                
+                result = 1;
+                msg = "다시 시도해주세요.";
+                return;          
+            } 
+            
+            // 조회된 레코드가 있으면 id 중복 응답 전송
+            if (dbdata.length != 0) {
+                console.dir(dbdata);
+                console.log("id 중복");
+                result = 1;
+            } else {
+                console.log("id 중복 아님");
+                result = 2;
+            }
+            res.send({result: result});
+            return;
+        });
+    });
+});
+module.exports = router;
