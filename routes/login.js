fefd7c5ab71c1f8b405aa09f58d1e2243f9ba00d
hjy <heojungyeon.97@gmail.com>
2021-12-21 20:40:40 +0900

    [macarontree] * 로그인 페이지
    ; 필요 없는 코드 삭제

--- routes/login.js
+++ routes/login.js
@@ -5,14 +5,10 @@
 
 var express = require('express');
 var router = express.Router();
-var session = require('express-session');
 
 
 router.get('/', function(req, res, next) {
     res.render('../views/login.ejs');
 });
 
-router.post('/', function(req, res, next) {
-    res.render('../views/login');
-});
 module.exports = router;
